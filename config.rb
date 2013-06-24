74fc696c99ac64425aceaf5055a6ad053159360c
stan <sidelnikov.stanislav@gmail.com>
2013-06-24 12:52:06 +0600

    "Removed de_ssidelnikov postfix to use with the custom domain - de.ssidelnikov.ru"

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/de_ssidelnikov/"
-http_images_path = "/de_ssidelnikov/images"
-http_fonts_path = "/de_ssidelnikov/fonts"
-css_dir = "public/de_ssidelnikov/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
